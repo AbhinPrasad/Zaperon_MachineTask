6a87b2d3e93b4f40019bfe057e9278df16d56bba
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-04-03 12:11:45 +0530

    folder structure -client side

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Footer, Form } from "../components";
+
+function Login() {
+	return (
+		<div>
+			<Form />
+			<Footer />
+		</div>
+	);
+}
+
+export default Login;
